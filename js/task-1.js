f3f063c8081b07e4690999415679f8f1e5c46cdf
Marianna Dei <d.marianna@icloud.com>
2024-02-06 22:39:03 +0200

    Updated index.html and files.js.

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,1 @@
-function slugify(title) {
-  let string;
-  string = title.toLowerCase();
-  let slugWord;
-  slugWord = string.split(' ');
-  const slug = slugWord.join('-');
-  return slug;
-}
-console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+
