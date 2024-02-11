c3f05f02b745b55ea0291624808d364be7ec797a
Marianna Dei <d.marianna@icloud.com>
2024-02-11 22:44:25 +0200

    Updated task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -6,11 +6,11 @@ const profile = {
         this.username = newName;
     },
     updatePlayTime(hours){
-       this.playTime += hours;
+        this.playTime += hours;
     },    
     getInfo() {
         return `${this.username} has ${this.playTime} active hours!`;
-      },
+    },
 };
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
